233594107967535c7c6d1b04353c12435b1745e7
Schenna43lp <91946010+Schenna43lp1@users.noreply.github.com>
2026-05-27 11:06:01 +0200

    Protect and harden print job page

--- app/job_add.php
+++ app/job_add.php
@@ -1,10 +1,10 @@
-<?php require 'config.php';
+<?php require 'config.php'; require 'auth.php'; require_login();
 $spools=$pdo->query('SELECT * FROM filament_spools')->fetchAll();
 if($_SERVER['REQUEST_METHOD']==='POST'){
 $used=(int)$_POST['material_used_g'];
 $pdo->beginTransaction();
 $stmt=$pdo->prepare('INSERT INTO print_jobs (spool_id,model_name,material_used_g,print_time_minutes,sale_price_eur) VALUES (?,?,?,?,?)');
-$stmt->execute([$_POST['spool_id'],$_POST['model_name'],$used,$_POST['print_time_minutes'],$_POST['sale_price_eur']]);
+$stmt->execute([$_POST['spool_id'],trim($_POST['model_name']),$used,$_POST['print_time_minutes'],$_POST['sale_price_eur']]);
 $upd=$pdo->prepare('UPDATE filament_spools SET remaining_weight_g=GREATEST(remaining_weight_g-?,0) WHERE id=?');
 $upd->execute([$used,$_POST['spool_id']]);
 $pdo->commit();
@@ -14,12 +14,12 @@ header('Location:index.php');exit;}
 <form method='post'>
 <select name='spool_id'>
 <?php foreach($spools as $s): ?>
-<option value='<?= $s['id'] ?>'><?= $s['brand'] ?> <?= $s['material'] ?> <?= $s['remaining_weight_g'] ?>g</option>
+<option value='<?= $s['id'] ?>'><?= htmlspecialchars($s['brand']) ?> <?= htmlspecialchars($s['material']) ?> <?= $s['remaining_weight_g'] ?>g</option>
 <?php endforeach; ?>
 </select><br><br>
-<input name='model_name' placeholder='Modell'><br><br>
-<input name='material_used_g' type='number' placeholder='Verbrauch g'><br><br>
-<input name='print_time_minutes' type='number' placeholder='Zeit'><br><br>
-<input name='sale_price_eur' type='number' step='0.01' placeholder='Preis'><br><br>
+<input name='model_name' placeholder='Modell' required><br><br>
+<input name='material_used_g' type='number' min='1' required placeholder='Verbrauch g'><br><br>
+<input name='print_time_minutes' type='number' min='0' placeholder='Zeit'><br><br>
+<input name='sale_price_eur' type='number' min='0' step='0.01' placeholder='Preis'><br><br>
 <button>Speichern</button>
 </form>
